12fd5bf91ed39acbff3c0b5c60e7e677627fc5a8
KimoiSama <tttferreteria@gmail.com>
2025-03-10 18:10:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,25 +6,29 @@ button.addEventListener("click", () => {
     count++;
     countDisplay.textContent = count;
 });
-
-// Tweets randomos de la cuenta
-const tweets = [
-    "Tweet random 1 aquí",
-    "Tweet random 2 aquí",
-    "Tweet random 3 aquí",
-    "Tweet random 4 aquí",
-    "Tweet random 5 aquí"
+// Cambiar el número de GIFs de 1 a 3 cada 3 segundos
+let gifContainer = document.getElementById('gif-container');
+let gifUrls = [
+    "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGg1bG1yOHhuaDFqcTBoZ3Y1cGZqdnBrNm4yenF6cGJ0d2k5MG4xaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/k0zSFlL0vwBVWRK8sT/giphy.gif",
+    "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGQwYm10eTlxcHh5NHc3Y2VqNGtpOWt1c2doeWp1d2FqMGZkMjVsMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SIAQookFwLMBtfV81U/giphy.gif",
+    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdWYxNXBrcWIwd2kwZ2t0bXo2eGpmcGE0aHFhbWY2eG13Ymo0NGtxYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DLso9u2jbnnu17ioK9/giphy.gif"
 ];
 
-let tweetIndex = 0;
-const tweetElements = document.querySelectorAll(".tweet");
+let gifIndex = 0;
 
-function changeTweet() {
-    tweetElements.forEach((el, index) => {
-        el.textContent = tweets[(tweetIndex + index) % tweets.length];
-    });
-    tweetIndex++;
+function changeGifs() {
+    gifContainer.innerHTML = ""; // Limpiar contenedor actual
+
+    let numGifs = gifIndex < 3 ? 1 : 3;
+    for (let i = 0; i < numGifs; i++) {
+        let gif = document.createElement('img');
+        gif.src = gifUrls[i];
+        gif.classList.add('gif');
+        gifContainer.appendChild(gif);
+    }
+
+    gifIndex = (gifIndex + 1) % 4;
 }
 
-setInterval(changeTweet, 5000);
-changeTweet();
+setInterval(changeGifs, 3000);
+changeGifs();  // Inicializar con 1 GIF
