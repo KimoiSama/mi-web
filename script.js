ec66174398fbf64a399621d80392cd87b5f62c4b
KimoiSama <tttferreteria@gmail.com>
2025-03-10 18:15:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,13 @@ const countDisplay = document.getElementById("clickCount");
 button.addEventListener("click", () => {
     count++;
     countDisplay.textContent = count;
-});
-// Cambiar el número de GIFs de 1 a 3 cada 3 segundos
-let gifContainer = document.getElementById('gif-container');
-let gifUrls = [
-    "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGg1bG1yOHhuaDFqcTBoZ3Y1cGZqdnBrNm4yenF6cGJ0d2k5MG4xaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/k0zSFlL0vwBVWRK8sT/giphy.gif",
-    "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGQwYm10eTlxcHh5NHc3Y2VqNGtpOWt1c2doeWp1d2FqMGZkMjVsMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SIAQookFwLMBtfV81U/giphy.gif",
-    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdWYxNXBrcWIwd2kwZ2t0bXo2eGpmcGE0aHFhbWY2eG13Ymo0NGtxYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DLso9u2jbnnu17ioK9/giphy.gif"
-];
-
+    const gifContainer = document.getElementById('gif-container');
 let gifIndex = 0;
+const gifUrls = [
+    "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjdwbXd4ZGF1OW50NjhiOGYzNG52Z3UzMTBjejRyNWF0czYwamJjdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/k0zSFlL0vwBVWRK8sT/giphy.gif",
+    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGQwYm10eTlxcHh5NHc3Y2VqNGtpOWt1c2doeWp1d2FqMGZkMjVsMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SIAQookFwLMBtfV81U/giphy.gif",
+    "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdWYxNXBrcWIwd2kwZ2t0bXo2eGpmcGE0aHFhbWY2eG13Ymo0NGtxYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DLso9u2jbnnu17ioK9/giphy.gif"
+];
 
 function changeGifs() {
     gifContainer.innerHTML = ""; // Limpiar contenedor actual
